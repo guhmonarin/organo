f3c71296cce31a8d81827f51bed518c9f21d8dfb
Gustavo Monarin <gustavomonarin11@gmail.com>
2023-08-28 16:29:55 -0300

    adicionando mais alguns componentes para dentro do projeto

--- organo/src/components/Formulario/index.js
+++ organo/src/components/Formulario/index.js
@@ -1,17 +1,35 @@
 import './formulario.css';
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
+
+const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    ' Inovação e Gestão'
+    ]
 
 const Formulario = () => {
     return (
         <section className = "formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador:</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholde="Digite seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>Criar card</Botao>
             </form>
         </section>
     )
+}
+
+const aoSalvar = (e) => {
+    e.preventDefault();
 }
 
 export default Formulario;
