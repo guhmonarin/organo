3df78c1755f797007497c7442cec2128a3a553de
Gustavo Monarin <gustavomonarin11@gmail.com>
2023-08-28 12:46:05 -0300

    adicionando os components do banner e formulario

---
+++ organo/src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import './banner.css';
+
+function Banner () {
+    return (
+        <header className = "banner">
+            <img src = "/img/banner.png" alt = "O banner principal do site"/>
+        </header>
+    )
+}
+
+export default Banner;
